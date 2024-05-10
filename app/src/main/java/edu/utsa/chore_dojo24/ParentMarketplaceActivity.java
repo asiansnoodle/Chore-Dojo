88962130976869bc676ac6edabee8cb6900325f1
Noodles <adrianalexis624@gmail.com>
2024-05-10 14:30:50 -0500

    added 4 buttons to parent marketplace page

--- app/src/main/java/edu/utsa/chore_dojo24/ParentMarketplaceActivity.java
+++ app/src/main/java/edu/utsa/chore_dojo24/ParentMarketplaceActivity.java
@@ -1,5 +1,6 @@
 package edu.utsa.chore_dojo24;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -83,6 +84,52 @@ public class ParentMarketplaceActivity extends ComponentActivity {
             }
         });
 
+        setupButtons();
+
     }
+
+    private void setupButtons() {
+        Button profileButton = (Button) findViewById(R.id.Profile);
+        Button houseButton = (Button) findViewById(R.id.Household);
+        Button marketButton = (Button) findViewById(R.id.Marketplace);
+        Button dashButton = (Button) findViewById(R.id.Dashboard);
+
+
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(HouseholdActivity.this, ProfileActivity.class);
+                // startActivity(intent);
+            }
+        });
+
+        houseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ParentMarketplaceActivity.this, HouseholdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        marketButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ParentMarketplaceActivity.this, ParentMarketplaceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        dashButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(HouseholdActivity.this, DashboardActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+
+    }
+
 
 }
